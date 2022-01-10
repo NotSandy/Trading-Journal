2c17fd3d2cbbab75fa0ebaeb164d33cc8362713d
NotSandy <sliu9982@gmail.com>
2022-01-10 01:00:53 -0500

    chore: add next-auth & prisma dependencies

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
+import TwitterProvider from "next-auth/providers/twitter";
+import RedditProvider from "next-auth/providers/reddit";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+    GoogleProvider({
+      // @ts-ignore
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      // @ts-ignore
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+    TwitterProvider({
+      // @ts-ignore
+      clientId: process.env.TWITTER_CLIENT_ID,
+      // @ts-ignore
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    }),
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET,
+    }),
+  ],
+});
