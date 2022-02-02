a8daa516b005b537bf7dc2dcb83f4516e31775f2
NotSandy <sliu9982@gmail.com>
2022-02-01 20:39:45 -0500

    fix: top trades widget ui

--- components/widgets/TopTrades.tsx
+++ components/widgets/TopTrades.tsx
@@ -13,8 +13,8 @@ const TopTrades = () => {
               <span>Top Trades</span>
             </div>
           </div>
-          <div className="flex flex-wrap basis-full gap-y-4">
-            <div className="flex items-center justify-between p-2 rounded-md basis-1/2 gap-x-4 bg-neutral-900">
+          <div className="flex flex-wrap gap-4 basis-full">
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
               <div className="flex items-center basis-2/3 gap-x-2">
                 <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
                   <Image
@@ -34,7 +34,241 @@ const TopTrades = () => {
                   </div>
                 </div>
               </div>
-              <div className="basis-1/3">
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
+                <span className="font-semibold text-success-300">
+                  +$1,564.58
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center justify-between flex-grow p-2 rounded-md basis-2/5 gap-x-4 bg-neutral-900">
+              <div className="flex items-center basis-2/3 gap-x-2">
+                <div className="relative w-12 h-12 border-2 rounded-full border-neutral-900">
+                  <Image
+                    src={`${session?.user?.image}`}
+                    alt="Profile_Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="flex-col whitespace-nowrap">
+                  <div className="font-semibold text-neutral-100">
+                    <span>AMD $140.00</span>
+                  </div>
+                  <div className="text-neutral-300">
+                    <span>2022-02-18 Call</span>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right basis-1/3">
                 <span className="font-semibold text-success-300">
                   +$1,564.58
                 </span>
